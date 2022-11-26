c433aa0ee0c0974cd886fff37fc84f14b7387c53
grimoh <dev@grimoh.net>
2022-11-26 23:46:37 +0900

    Add 1/4 dir

---
+++ 1/4/cjs-math.js
@@ -0,0 +1,19 @@
+// 有効
+
+// module.exports.add = (a, b) => a + b;
+// module.exports.subtract = (a, b) => a - b;
+
+// module.exports = {
+// 	add: (a, b) => a + b,
+// 	subtract: (a, b) => a - b,
+// };
+
+// exports.add = (a, b) => a + b
+// exports.subtract = (a, b) => a - b
+
+// 無効
+
+// exports = {
+// 	add: (a, b) => a + b,
+// 	subtract: (a, b) => a - b,
+// }
